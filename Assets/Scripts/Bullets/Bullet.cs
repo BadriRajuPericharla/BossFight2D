dce1e3b1a154cfbf2292a7d636d1dbef0874c7bc
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-08-11 15:05:38 +0530

    Optimized game and imported effects

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -6,14 +6,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]private float Speed=20f;
-    private EnemyDamage enemyDamage;
     Rigidbody2D rb;
     
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
         rb.velocity=transform.right*Speed;
-        enemyDamage=GameObject.FindGameObjectWithTag("HitPoint").GetComponent<EnemyDamage>();
         
     }
     void OnTriggerEnter2D(Collider2D collision)
