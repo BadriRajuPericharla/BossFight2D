b7f854346f67ae35b20b0875ae64841e23e63657
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-09-02 12:23:04 +0530

    Fixed Shield Glitch

--- Assets/Scripts/Player/PlayerShield.cs
+++ Assets/Scripts/Player/PlayerShield.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Unity.VisualScripting;
 
 public class PlayerShield : MonoBehaviour
 {
@@ -19,39 +20,57 @@ public class PlayerShield : MonoBehaviour
     [SerializeField]private GameObject playershield;
     [SerializeField]private Image image;
     public bool shieldActivated=false;
+    private bool resetShield=false;
     void Start()
     {
         playershield.SetActive(false);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !shieldActivated)
         {
             StartCoroutine(ActivateShield());
         }
     }
     public void ShieldButton()
     {
-        StartCoroutine(ActivateShield());
-        StartCoroutine(ResetShield());
+        if (!resetShield)
+        {
+            StartCoroutine(ActivateShield());
+        }
     }
     IEnumerator ActivateShield()
     {
+        image.enabled = true;
         playershield.SetActive(true);
-        shieldActivated=true;
-        yield return new WaitForSeconds(3);
+        shieldActivated = true;
+
+        float duration = 3f;
+        float time = 0f;
+
+        image.fillAmount = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            image.fillAmount = time / duration;
+
+            yield return null;
+        }
+
+        image.fillAmount = 1f;
+
         playershield.SetActive(false);
-        shieldActivated=false;
+        shieldActivated = false;
 
-        
-
+        StartCoroutine(ResetShield());
     }
     IEnumerator ResetShield()
     {
         image.enabled=true;
         float cooldown = -13f;
         float time = cooldown;
-
+        resetShield=true;
         image.fillAmount = 1;
 
         while (time < 0f)
@@ -62,6 +81,7 @@ public class PlayerShield : MonoBehaviour
         }
 
         image.fillAmount = 0;
+        resetShield=false;
         image.enabled=false;
     }
 }
